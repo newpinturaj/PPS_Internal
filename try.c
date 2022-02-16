c9fb1d09542ab96e4ba481fa7618e81988c8584b
PINTU RAJ <newpinturaj@gmail.com>
2022-02-17 01:57:06 +0530

    file changed

--- try.c
+++ try.c
@@ -1,21 +1,9 @@
-
 #include <stdio.h>
-int check(int* brr);
 
-int check(int*brr)
-{
-    for(int i = 0; i<5; i++)
-    {
-        printf("numbers are: %d\n", brr[i]);
-    }
-    return 5;
-}
 
 int main()
 {
-    int arr[5], y;
-    for(int i = 0; i<5; i++)
-    scanf("%d", &arr[i]);
-    y = check(arr);
-    printf("\ncheck: %d and y = %d\n", check(arr), y);
+    int a = 4, b = 3;   
+    int x = a++*b*++a;
+    printf("%d\n", a);
 }
